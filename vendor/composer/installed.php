b951452bf39e9014ce92272e4158ee02eca5ba69
lalaPlus <lala.plus123@gmail.com>
2021-08-18 19:11:25 +0900

    Add template engine for PHP: Smarty
    Composer package(vendor) installed with Smarty.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '64c64961f62dc3cf54d298c0b25511343a06b862',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '64c64961f62dc3cf54d298c0b25511343a06b862',
+            'dev_requirement' => false,
+        ),
+        'smarty/smarty' => array(
+            'pretty_version' => 'v3.1.39',
+            'version' => '3.1.39.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smarty/smarty',
+            'aliases' => array(),
+            'reference' => 'e27da524f7bcd7361e3ea5cdfa99c4378a7b5419',
+            'dev_requirement' => false,
+        ),
+    ),
+);
