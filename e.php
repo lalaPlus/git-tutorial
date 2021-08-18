0ffa7ece549a6d0d76a30b29d958303e7ff67b20
lalaPlus <lala.plus123@gmail.com>
2021-08-18 19:44:54 +0900

    Fix e.php

--- e.php
+++ e.php
@@ -1,4 +1,4 @@
 <?php
   function e($super_globals) {
-   htmlspecialchars($super_globals, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+   return htmlspecialchars($super_globals, ENT_QUOTES | ENT_HTML5, 'UTF-8');
   }
