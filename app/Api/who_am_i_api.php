d4f5beea273225fbd924633683fec7a9d258745c
lalaPlus <lala.plus123@gmail.com>
2021-08-19 21:38:01 +0900

    Add directory, Move existing	files
    Added directory: app
    Moved files:
    	/index.html → /app/Views/index.html
    	/Guest.php → /app/Library/Guest.php
    	/e.php → /app/Library/e.php
    	/who_am_i_api.php → /app/Api/who_am_i_api.php

---
+++ app/Api/who_am_i_api.php
@@ -0,0 +1,18 @@
+<?php
+
+ namespace Api;
+
+ require_once '../Library/e.php';
+ require_once '../Library/Guest.php';
+
+ use Library\Guest;
+ use function Library\e;
+
+ $guest = new Guest(e($_POST['name']), ['y' => e($_POST['birth_year']), 'm' => e($_POST['birth_month']), 'd' => e($_POST['birth_day'])]);
+
+ $msg = <<<EOD
+ お名前: {$guest->name} <br />
+ 誕生日: {$guest->birth['y']}/{$guest->birth['m']}/{$guest->birth['d']}
+ EOD;
+
+ print $msg;
