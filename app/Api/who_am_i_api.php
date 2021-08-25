f9e9c9fb6bb9624828912e73fd1a31aaf3db2dac
lalaPlus <lala.plus123@gmail.com>
2021-08-25 21:51:30 +0900

    Add var '$name $birth $calc_age'

--- app/Api/who_am_i_api.php
+++ app/Api/who_am_i_api.php
@@ -12,12 +12,15 @@
 
  $guest = new Guest(e($_POST['name']), ['y' => e($_POST['birth_year']), 'm' => e($_POST['birth_month']), 'd' => e($_POST['birth_day'])]);
 
- $msg = <<<EOD
- お名前: {$guest->name}
- 誕生日: {$guest->birth['y']}/{$guest->birth['m']}/{$guest->birth['d']}
- EOD;
+ $name = $guest->name;
+ $birth = [
+  'y' => $guest->birth['y'],
+  'm' => $guest->birth['m'],
+  'd' => $guest->birth['d'],
+ ];
+ $calc_age = $guest->calcAge();
 
  // ビュー('hogehoge')に渡す
  $blade = new BladeOne(__DIR__ . '/../Views', __DIR__ . '/../Views/template_cache', BladeOne::MODE_AUTO);
 
- echo $blade->run('hogehuga', ['msg' => $msg]);
+ echo $blade->run('hogehuga', ['name' => $name, 'birth' => $birth, 'calc_age' => $calc_age]);
